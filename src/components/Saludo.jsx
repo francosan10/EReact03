8b26d7cb2aa24314053da1a377f215aa96c28743
Franco <lmassa.fms@gmail.com>
2024-02-05 15:41:24 -0300

    agregamos funcion onclick y use state

--- src/components/Saludo.jsx
+++ src/components/Saludo.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+
+
 const MyFriend = ({ name }) => {
+    const [mensaje, setMensaje] = useState("(from changed state)");
+    
+    const cambiar = ()=> {
+        setMensaje("(changed state)")
+    }
+
     return (
       <section className="container text-center my-3">
-          <h1 className="display-2">Hello {name}!</h1>
-          <button className="btn btn-primary">Click me!</button>
+          <h1 className="display-2">Hello {name} {mensaje}!</h1>
+          <button className="btn btn-primary" onClick={cambiar}>Click me!</button>
       </section>
     );
   };
